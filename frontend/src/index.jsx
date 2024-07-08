55f875a197ea90e3b219c1e9e581e5b344db715c
Quang Thoai Vu <vuthoaivt@gmail.com>
2024-07-08 11:01:53 +0700

    cau truc thu muc fronntend

---
+++ frontend/src/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/App.jsx";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import reducers from "./redux/reducers/index.js";
+import createSagaMiddleware from "redux-saga";
+import mySaga from "./redux/sagas/index.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleWare) => {
+    return getDefaultMiddleWare({ thunk: false }).prepend(sagaMiddleware);
+  },
+});
+
+sagaMiddleware.run(mySaga);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
