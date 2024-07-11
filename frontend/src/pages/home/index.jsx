0d17e5cbc4a1b21ae21e00b32b1efbfb09d9dacf
Quang Thoai Vu <vuthoaivt@gmail.com>
2024-07-11 11:32:18 +0700

    create appbar

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Grid, Typography, LinearProgress, Box } from "@mui/material";
+
+import MyAppBar from "../../components/Appbar";
+import { getAllCars } from "../../redux/actions";
+import { productState$ } from "../../redux/selector";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const [isLoading, setLoading] = React.useState(false);
+  const cars = useSelector(productState$)?.cars;
+
+  React.useEffect(() => {
+    dispatch(getAllCars.getAllCarsRequest());
+  }, []);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      if (cars?.length > 0) {
+        setLoading(false);
+      }
+    }, 3000);
+  }, [cars]);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Grid container spacing={2}>
+      {isLoading ? (
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          height="101.6vh"
+        >
+          <Box sx={{ width: "20%" }}>
+            <LinearProgress />
+          </Box>
+          <Typography variant="h5" sx={{ marginTop: "10px" }}>
+            Loading...
+          </Typography>
+        </Grid>
+      ) : (
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={12} width="100%">
+            <MyAppBar />
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
   );
 }
